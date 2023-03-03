8f1161c65d0440c44761b96a7c8b1807ab9312fc
BowenD-UCB <84425382+BowenD-UCB@users.noreply.github.com>
2023-03-03 00:27:16 -0800

    solved bug that caused circular import

--- chgnet/model/__init__.py
+++ chgnet/model/__init__.py
@@ -1,4 +1,4 @@
-from .dynamics import CHGNetCalculator, MolecularDynamics, StructOptimizer
 from .model import CHGNet
+from .dynamics import CHGNetCalculator, MolecularDynamics, StructOptimizer
 
 __all__ = ["CHGNet", "StructOptimizer", "MolecularDynamics", "CHGNetCalculator"]
