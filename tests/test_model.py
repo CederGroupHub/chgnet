8ef4e832f2e900c1b2cf571e95533bc16aa4b118
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-13 11:59:44 -0700

    add test_predict_structure()

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import numpy as np
+import pytest
 from pymatgen.core import Structure
 from pytest import mark
 
@@ -25,7 +27,7 @@ def test_model(
     num_angular: int,
     n_conv: int,
     composition_model: str,
-):
+) -> None:
     model = CHGNet(
         atom_fea_dim=atom_fea_dim,
         bond_fea_dim=bond_fea_dim,
@@ -39,3 +41,26 @@ def test_model(
     assert list(out) == ["atoms_per_graph", "e"]
     assert out["atoms_per_graph"].shape == (1,)
     assert out["e"] < 0
+
+
+def test_predict_structure() -> None:
+    model = CHGNet.load()
+    out = model.predict_structure(structure)
+
+    assert sorted(out) == ["e", "f", "m", "s"]
+    assert out["e"] == pytest.approx(-7.37159, abs=1e-4)
+    assert len(out["f"]) == len(structure)
+    assert len(out["m"]) == len(structure)
+    assert out["m"] == pytest.approx(
+        [0.00521, 0.00521, 3.85728, 3.85729, 0.02538, 0.03706, 0.03706, 0.02538],
+        abs=1e-4,
+    )
+    assert len(out["s"]) == 3
+    stress = np.array(
+        [
+            [3.3677614e-01, -1.9665707e-07, -5.6416429e-06],
+            [4.9939729e-07, 2.4675032e-01, 1.8549043e-05],
+            [-4.0414070e-06, 1.9096897e-05, 4.0323928e-02],
+        ]
+    )
+    assert out["s"] == pytest.approx(stress, abs=1e-4)
