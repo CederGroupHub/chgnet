010c7270f2531bef6b113193579492bdad7e09fe
lbluque <lbluque@berkeley.edu>
2023-03-14 18:45:44 -0700

    refactor CrystalGraphConverter into its own file

--- chgnet/graph/__init__.py
+++ chgnet/graph/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from chgnet.graph.crystalgraph import CrystalGraph, CrystalGraphConverter, BatchedGraph
-from chgnet.graph.graph import Graph, Node
+from chgnet.graph.crystalgraph import CrystalGraph, BatchedGraph
+from chgnet.graph.converter import CrystalGraphConverter
 
-__all__ = ["CrystalGraph", "CrystalGraphConverter", "BatchedGraph", "Graph", "Node"]
+__all__ = ["CrystalGraph", "BatchedGraph", "CrystalGraphConverter"]
