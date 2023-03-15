eb726a703dc42380b70a93b8f5386a700db0c37d
lbluque <lbluque@berkeley.edu>
2023-03-14 18:32:49 -0700

    move BatchedGraph to graph module

--- chgnet/graph/__init__.py
+++ chgnet/graph/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from chgnet.graph.crystalgraph import CrystalGraph, CrystalGraphConverter
+from chgnet.graph.crystalgraph import CrystalGraph, CrystalGraphConverter, BatchedGraph
 from chgnet.graph.graph import Graph, Node
 
-__all__ = ["CrystalGraph", "CrystalGraphConverter", "Graph", "Node"]
+__all__ = ["CrystalGraph", "CrystalGraphConverter", "BatchedGraph", "Graph", "Node"]
