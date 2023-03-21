5c079ad27036ce0c233b80f07df7b92a980a928a
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-21 14:49:13 -0700

    add AverageMeter doc strings

--- chgnet/utils/utils.py
+++ chgnet/utils/utils.py
@@ -14,15 +14,20 @@ class AverageMeter:
     """Computes and stores the average and current value."""
 
     def __init__(self) -> None:
+        """Initialize the meter."""
         self.reset()
 
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+    def reset(self) -> None:
+        """Reset the meter value, average, sum and count to 0."""
+        self.val = self.avg = self.sum = self.count = 0.0
 
-    def update(self, val, n=1):
+    def update(self, val: float, n: int = 1) -> None:
+        """Update the meter value, average, sum and count.
+
+        Args:
+            val (float): New value to be added to the running average.
+            n (int, optional): Number of times the value is added. Default = 1.
+        """
         self.val = val
         self.sum += val * n
         self.count += n
