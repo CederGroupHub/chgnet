3912f4ee7a751e205e5e39a3c7a9c809c57096c4
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-25 08:03:07 -0700

    guard against AttributeError: module 'torch.backends' has no attribute 'mps' on non-macOS

--- tests/test_relaxation.py
+++ tests/test_relaxation.py
@@ -35,7 +35,7 @@ def test_relaxation():
 
 
 no_cuda = mark.skipif(not torch.cuda.is_available(), reason="No CUDA device")
-no_mps = mark.skipif(not torch.backends.mps.is_available(), reason="No MPS device")
+no_mps = mark.skipif(not hasattr(torch.backends, "mps"), reason="No MPS device")
 
 
 @mark.parametrize(
