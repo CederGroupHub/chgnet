5ac66722e026089542df2718501d933a74be8e34
BowenD-UCB <84425382+BowenD-UCB@users.noreply.github.com>
2023-05-03 23:18:26 -0700

    added tests to dataset and trainer

---
+++ tests/test_dataset.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import numpy as np
+import pytest
+import torch
+from pymatgen.core import Lattice, Structure
+
+from chgnet.data.dataset import StructureData, get_train_val_test_loader
+from chgnet.graph import CrystalGraph
+
+lattice = Lattice.cubic(4)
+species = ["Na", "Cl"]
+coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+NaCl = Structure(lattice, species, coords)
+
+
+@pytest.fixture
+def structure_data() -> StructureData:
+    """Create a graph with 3 nodes and 3 directed edges."""
+    structures, energies, forces, stresses, magmoms = [], [], [], [], []
+    for _ in range(100):
+        struct = NaCl.copy()
+        struct.perturb(0.1)
+        structures.append(struct)
+        energies.append(np.random.random(1))
+        forces.append(np.random.random([2, 3]))
+        stresses.append(np.random.random([3, 3]))
+        magmoms.append(np.random.random([2, 1]))
+    data = StructureData(
+        structures=structures,
+        energies=energies,
+        forces=forces,
+        stresses=stresses,
+        magmoms=magmoms,
+    )
+    return data
+
+
+def test_structure_data(structure_data: StructureData) -> None:
+    get_one = structure_data[0]
+    assert isinstance(get_one[0], CrystalGraph)
+    assert isinstance(get_one[1], dict)
+    assert isinstance(get_one[1]["e"], torch.Tensor)
+    assert isinstance(get_one[1]["f"], torch.Tensor)
+    assert isinstance(get_one[1]["s"], torch.Tensor)
+    assert isinstance(get_one[1]["m"], torch.Tensor)
+
+
+def test_data_loader(structure_data: StructureData) -> None:
+    train_loader, val_loader, test_loader = get_train_val_test_loader(
+        structure_data, batch_size=16, train_ratio=0.9, val_ratio=0.05
+    )
+    graphs, targets = next(iter(train_loader))
+    assert isinstance(train_loader, torch.utils.data.DataLoader)
+    assert len(graphs) == 16
+    assert isinstance(graphs[0], CrystalGraph)
+    assert targets["e"].shape == (16,)
+    assert len(targets["f"]) == 16
+    assert targets["f"][0].shape == (2, 3)
+    assert len(targets["s"]) == 16
+    assert targets["s"][0].shape == (3, 3)
+    assert len(targets["m"]) == 16
+    assert targets["m"][0].shape == (2, 1)
---
+++ tests/test_trainer.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import numpy as np
+from pymatgen.core import Lattice, Structure
+
+from chgnet.data.dataset import StructureData, get_train_val_test_loader
+from chgnet.model import CHGNet
+from chgnet.trainer import Trainer
+
+lattice = Lattice.cubic(4)
+species = ["Na", "Cl"]
+coords = [[0, 0, 0], [0.5, 0.5, 0.5]]
+NaCl = Structure(lattice, species, coords)
+structures, energies, forces, stresses, magmoms = [], [], [], [], []
+for _ in range(100):
+    struct = NaCl.copy()
+    struct.perturb(0.1)
+    structures.append(struct)
+    energies.append(np.random.random(1))
+    forces.append(np.random.random([2, 3]))
+    stresses.append(np.random.random([3, 3]))
+    magmoms.append(np.random.random(2))
+data = StructureData(
+    structures=structures,
+    energies=energies,
+    forces=forces,
+    stresses=stresses,
+    magmoms=magmoms,
+)
+
+
+def test_trainer(tmp_path) -> None:
+    chgnet = CHGNet.load()
+    train_loader, val_loader, test_loader = get_train_val_test_loader(
+        data, batch_size=16, train_ratio=0.9, val_ratio=0.05
+    )
+    trainer = Trainer(
+        model=chgnet,
+        targets="efsm",
+        optimizer="Adam",
+        criterion="MSE",
+        learning_rate=1e-2,
+        epochs=5,
+    )
+    dir_name = "test_tmp_dir"
+    test_dir = tmp_path / dir_name
+    trainer.train(train_loader, val_loader, save_dir=test_dir)
+    assert test_dir.is_dir(), "Training dir was not created"
+
+    saved_weight = [f for f in test_dir.iterdir() if f.name.startswith("epoch")]
+    assert len(saved_weight) == 1
