b89fd48d2fe34b8f619694dbf50ae150a619e3c3
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-06-28 14:17:11 -0700

    use pytest.mark.parametrize to keep relaxation test DRY

--- tests/test_relaxation.py
+++ tests/test_relaxation.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from typing import Literal
+
+import pytest
 import torch
 from pymatgen.core import Structure
 from pytest import approx, mark, param
@@ -11,44 +14,15 @@ from chgnet.model import CHGNet, StructOptimizer
 structure = Structure.from_file(f"{ROOT}/examples/o-LiMnO2_unit.cif")
 
 
-def test_relaxation_legacy_converter():
+@pytest.mark.parametrize("algorithm", ["legacy", "fast"])
+def test_relaxation(algorithm: Literal["legacy", "fast"]):
     chgnet = CHGNet.load()
-    converter_legacy = CrystalGraphConverter(
-        atom_graph_cutoff=5, bond_graph_cutoff=3, algorithm="legacy"
+    converter = CrystalGraphConverter(
+        atom_graph_cutoff=5, bond_graph_cutoff=3, algorithm=algorithm
     )
-    assert converter_legacy.algorithm == "legacy"
+    assert converter.algorithm == algorithm
 
-    chgnet.graph_converter = converter_legacy
-    relaxer = StructOptimizer(model=chgnet)
-    result = relaxer.relax(structure, verbose=True)
-    assert list(result) == ["final_structure", "trajectory"]
-
-    traj = result["trajectory"]
-    # make sure trajectory has expected attributes
-    assert list(traj.__dict__) == [
-        "atoms",
-        "energies",
-        "forces",
-        "stresses",
-        "magmoms",
-        "atom_positions",
-        "cells",
-    ]
-    assert len(traj) == 4
-
-    # make sure final structure is more relaxed than initial one
-    assert traj.energies[0] > traj.energies[-1]
-    assert traj.energies[-1] == approx(-58.972927)
-
-
-def test_relaxation_fast_converter():
-    chgnet = CHGNet.load()
-    converter_fast = CrystalGraphConverter(
-        atom_graph_cutoff=5, bond_graph_cutoff=3, algorithm="fast"
-    )
-    assert converter_fast.algorithm == "fast"
-
-    chgnet.graph_converter = converter_fast
+    chgnet.graph_converter = converter
     relaxer = StructOptimizer(model=chgnet)
     result = relaxer.relax(structure, verbose=True)
     assert list(result) == ["final_structure", "trajectory"]
