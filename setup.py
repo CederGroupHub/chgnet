7399299f7e40da8f2356e24fef56cbaebf55f043
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-09-17 19:36:05 -0700

    setup.py use ext_modules instead of custom BuildExt

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 from __future__ import annotations
 
 from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
 
+ext_modules = [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"])]
 
-class BuildExt(build_ext):
-    """Custom build extension."""
-
-    def finalize_options(self):
-        """Override finalize_options to ensure we don't run cythonize when making
-        a source distribution.
-        """
-        import Cython.Build
-
-        self.distribution.ext_modules = Cython.Build.cythonize(
-            [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"])]
-        )
-        super().finalize_options()
-
-
-setup(cmdclass={"build_ext": BuildExt})
+setup(ext_modules=ext_modules, setup_requires=["Cython"])
