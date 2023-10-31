8948863f16c401f9f67b0dab6a3a91c94f535a83
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-31 16:15:22 -0700

    Add `@property` `n_params` (#94)
    * CHGNet add property n_params
    
    * add version and n_params properties to CHGNetCalculator and StructOptimizer
    
    * test version and n_params on CHGNet, CHGNetCalculator, StructOptimizer

--- tests/test_model.py
+++ tests/test_model.py
@@ -225,17 +225,25 @@ def test_as_to_from_dict() -> None:
     assert model_3.todict() == to_dict
 
 
-def test_model_load(capsys: pytest.CaptureFixture) -> None:
+def test_model_load_version_params(capsys: pytest.CaptureFixture) -> None:
     model = CHGNet.load()
     assert model.version == "0.3.0"
+    assert model.n_params == 412_525
     stdout, stderr = capsys.readouterr()
-    assert stdout == f"CHGNet v{model.version} initialized with 412,525 parameters\n"
+    assert (
+        stdout
+        == f"CHGNet v{model.version} initialized with {model.n_params:,} parameters\n"
+    )
     assert stderr == ""
 
     model = CHGNet.load(model_name="0.2.0")
     assert model.version == "0.2.0"
+    assert model.n_params == 400_438
     stdout, stderr = capsys.readouterr()
-    assert stdout == f"CHGNet v{model.version} initialized with 400,438 parameters\n"
+    assert (
+        stdout
+        == f"CHGNet v{model.version} initialized with {model.n_params:,} parameters\n"
+    )
     assert stderr == ""
 
     model_name = "0.1.0"  # invalid
