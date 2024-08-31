180ffeafe43921827cc8fb11dbf57488d735bb35
alex <ahoffman2011@gmail.com>
2024-08-31 04:37:49 -0400

    Dispersion (#192)
    * ENH: add dispersion tutorial
    
    * STY: format dispersion notebook
    
    * add extra deps set dispersion = ["dftd4>=3.6", "torch-dftd>=0.4"]
    
    also bump ruff
    
    * tweak dispersion.ipynb var names
    
    ---------
    
    Co-authored-by: alex <ajhoff29@mit.edu>
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from pymatgen.core import Structure
 from chgnet import ROOT
 
 
-@pytest.fixture()
+@pytest.fixture
 def li_mn_o2() -> Structure:
     return Structure.from_file(f"{ROOT}/examples/mp-18767-LiMnO2.cif")
