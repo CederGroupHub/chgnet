e4ceb6a0d63c2a998ee6744b94343469e388791b
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-09-11 22:57:49 +0800

    Revert "maybe fix setup.py ModuleNotFoundError: No module named 'numpy'"
    This reverts commit 45a1fd75dcbb622a4d472d5004371cd1a87b6d69.

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from __future__ import annotations
 
+import numpy as np
 from setuptools import Extension, setup
 
 ext_modules = [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"])]
 
-
-def lazy_numpy_include() -> str:
-    """Get the numpy include directory lazily."""
-    import numpy as np
-
-    return np.get_include()
-
-
 setup(
     ext_modules=ext_modules,
     setup_requires=["Cython"],
-    include_dirs=[lazy_numpy_include()],
+    include_dirs=[np.get_include()],
 )
