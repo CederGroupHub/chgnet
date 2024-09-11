1d0b1530b93d5337f1f2a00472b00e50c108fe5d
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-09-11 21:57:12 +0800

    include numpy c header

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+import numpy as np
 from setuptools import Extension, setup
 
 ext_modules = [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"])]
 
-setup(ext_modules=ext_modules, setup_requires=["Cython"])
+setup(
+    ext_modules=ext_modules,
+    setup_requires=["Cython"],
+    include_dirs=[np.get_include()],
+)
